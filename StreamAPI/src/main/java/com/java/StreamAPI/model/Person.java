782cf6f6fe4c953ed052fda95b850fea701471d1
hoangdz1005 <nnh10052002@gmail.com>
2023-08-19 21:33:39 +0700

    xử lí dữ liệu sử dụng stream 💎

---
+++ StreamAPI/src/main/java/com/java/StreamAPI/model/Person.java
@@ -0,0 +1,40 @@
+package com.java.StreamAPI.model;
+
+
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private String country;
+    private Integer age;
+
+    public Person(String name, String country, Integer age) {
+        this.name = name;
+        this.country = country;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
