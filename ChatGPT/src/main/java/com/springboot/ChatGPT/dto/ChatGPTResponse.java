7f9b9ce369a7c98654167ed51a62eccc32de5a2c
hoangdz1005 <nnh10052002@gmail.com>
2023-08-19 21:52:32 +0700

    tích hợp chat GPT vào ứng dụng 🔎

---
+++ ChatGPT/src/main/java/com/springboot/ChatGPT/dto/ChatGPTResponse.java
@@ -0,0 +1,46 @@
+package com.springboot.ChatGPT.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGPTResponse {
+    private List<Choice> choices;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+        private int index;
+        private Message message;
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public Message getMessage() {
+            return message;
+        }
+
+        public void setMessage(Message message) {
+            this.message = message;
+        }
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
+}
